7ddf3e70c4b11f654affbfd8d4e3d5fb46386832
SATYAM PANDEY <146743104+psatyam2@users.noreply.github.com>
2025-10-22 09:41:54 +0530

    Delete .github/apiConfig.js

--- .github/apiConfig.js
+++
@@ -1,6 +0,0 @@
-
-// Export your API key and URL
-
-export const API_KEY = "AIzaSyDrfiecALDf5oVqvwTkeLLowf544NC3Zvo";
-
-export const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
